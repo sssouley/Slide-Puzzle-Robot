ac8b255ce7ab04e7ec2fb1fac32d023665ec2fce
sssouley <souleymane.cissokho@tufts.edu>
2025-05-03 15:35:38 -0400

    Add files via upload

---
+++ MQTT_Robot_2.py
@@ -0,0 +1,51 @@
+import time
+from XRPLib.differential_drive import DifferentialDrive
+from MQTT.mqttconnect import *
+
+# Add SSID and PW in config.txt
+# Add MQTT broker IP in config.txt and mqttconnect.py
+
+robot2 = DifferentialDrive.get_default_differential_drive()
+
+c = connect_mqtt()
+c.ping()
+
+if False: #publishing
+    c.publish("topic/message", "Start driving", retain=True)
+    drivetrain.set_speed(10,10)
+    time.sleep(2)
+    c.publish("topic/message", "Stop motors", retain=True)
+    drivetrain.set_speed(0,0)
+    c.publish("topic/data", str(20), retain=True)
+else: #subscribing # julian is going to send things over certain topics which are exclusive to each robot
+    #robots constantly listneing, only moves when spoken to
+    def handle_message(dx, dy):
+        try:
+            # Robot 2
+            dxMovement = int(dx)
+            dyMovement = int(dy)
+            robot2.stop()
+            while abs(dxMovement) > 20:
+                if dxMovement > 0:
+                    robot2.set_effort(0.2,0.2) # walking until it's done
+                if dxMovement < 0:
+                    robot2.set_effort(-0.2, -0.2)
+            robot2.stop()
+            time.sleep(3)
+            while abs(dyMovement) > 20:
+                if dyMovement > 0:
+                    robot2.turn(90, max_effort=0.5)
+                    robot2.set_effort(0.2,0.2)
+                if dyMovement < 0:
+                    robot2.turn(-90, max_effort= 0.5)
+            robot2.stop()
+        except:
+            print("Exception parsing")
+    
+    c.set_callback(handle_message)
+    c.subscribe("SlidePuzzle/send/2") #can be re-named to any topic name and any data name e.g., nemitz/kinematics
+    while True:
+        try:
+            c.wait_msg()
+        except:
+            print("exception")
